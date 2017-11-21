0d39b70f774fa3bb9b2f4a107eda8c5417ac3121
Jacques Lybarger-Martel <jacqueslybargermar@students.berkeley.net>
2017-11-21 11:36:36 -0800

    as

--- Hangman.js
+++ Hangman.js
@@ -1,35 +1,30 @@
 
 alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",",p","q","r","s","t","u","v","w","x","y","z"];
-secretwords= ["stanza", "anagrams", "gorilla", "cheese", "mbappe", "messi"];
-function startGame(){
-    document.getElementById("underline").innerHTML = "";
+categories= ["Animals", "Difficult", "Cheeses", "Footballers"];
+Animals= ["Gorilla", "Elephant", "Shark"];
+Difficult= ["Plethora","Xylophone", "Catatonic", "Disagreement", "Gigabyte"];
+Cheeses= ["Brie", "Manchego", "Gruyere", "Mozzarella", "Parmesan"];
+Footballers= ["Mbappe", "Messi", "Lemar", "Varane", "Koscielny", "Lloris"];
+document.getElementById("word").innerHTML = chooseWords();
+document.getElementById("underline").innerHTML = startGame();
 
-    secretwords= secretwords[Math.floor(Math.random()*secretwords.length)];
+function chooseWords(){
+     if(categories[3]==true){
+         return Footballers[Math.floor(Math.random() * Footballers.length)];
+     }else if(categories[2]==true){
+         return Cheeses[Math.floor(Math.random() * Cheeses.length)];
+     }else if(categories[1]==true){
+         return Difficult[Math.floor(Math.random() * Difficult.length)];
+     }else {
+         return Animals[Math.floor(Math.random() * Animals.length)];
+     }
+}
+
+function startGame(){
     var displayedLetters = "";
-    for (var i=0; i<secretwords.length; i++){
-        displayedLetters = "_";
+    for (var i=0; i<categories.length; i++){
+        displayedLetters += "_ ";
     }
     document.getElementById("underline").innerHTML = displayedLetters;
-
-}
-
-function Hangman(){
-    letter = document.getElementById("letter").value;
-    document.getElementById("Guess").innerHTML = letter;
-}
-
-
-
-
-
-
-function response(){
-    for (var i=0; i<alphabet.length-1; i++) {
-        if (alphabet[i]==letter) {
-            return letter;
-        }else{
-            return "Please enter a letter.";
-        }
-    }
 }
 
