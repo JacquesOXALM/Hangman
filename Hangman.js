80f7a11564e56f15f285c1dc3aed709ac2b31070
Jacques Lybarger-Martel <jacqueslybargermar@students.berkeley.net>
2017-12-01 11:38:41 -0800

    as

--- Hangman.js
+++ Hangman.js
@@ -1,28 +1,50 @@
 
-alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",",p","q","r","s","t","u","v","w","x","y","z"];
-Animals= ["Gorilla", "Elephant", "Shark"];
-Difficult= ["Plethora","Xylophone", "Catatonic", "Disagreement", "Gigabyte"];
-Cheeses= ["Brie", "Manchego", "Gruyere", "Mozzarella", "Parmesan"];
-Footballers= ["Mbappe", "Messi", "Lemar", "Varane", "Koscielny", "Lloris"];
-document.getElementById("word").innerHTML = chooseWords();
-document.getElementById("underline").innerHTML = startGame();
-categories= [Animals, Difficult, Cheeses, Footballers];
-var category = categories[document.getElementById("word").value];
-var cat = parseInt(category);
+
+var Animals= ["Gorilla", "Elephant", "Shark"];
+var Difficult= ["Plethora","Xylophone", "Catatonic", "Disagreement", "Gigabyte"];
+var Cheeses= ["Brie", "Manchego", "Gruyere", "Mozzarella", "Parmesan"];
+var Footballers= ["Mbappe", "Messi", "Lemar", "Varane", "Koscielny", "Lloris"];
+// document.getElementById("word").innerHTML = chooseWord();
+// document.getElementById("underline").innerHTML = chooseWord();
+var categories= [Animals, Difficult, Cheeses, Footballers];
+var word="";
+var guessesLeft= 15;
+var guessedLetter = [];
+var displayedLetters = "";
+
+function chooseWord() {
+    var x = document.getElementById("categories").value;
+    var category = categories[x];
+    console.log(categories);
+    word = category[Math.floor(Math.random() * category.length)];
 
 
-
-function startGame(){
-    if(cat==1){
-       word = Animals[Math.floor(Math.random() * Animals.length];
-    }else if(cat==2){
-
-    }
-
-    var displayedLetters = "";
-    for (var i=0; i<categories.length; i++){
+    for (var i = 0; i < word.length; i++) {
         displayedLetters += "_ ";
     }
+    console.log(word);
     document.getElementById("underline").innerHTML = displayedLetters;
+}
+
+function guessLetter(){
+    var x= document.getElementById("letter");
+    var guess = document.getElementById("letter").value;
+    guessedLetter.push(guess);
+    var displayGuess= "";
+    for(var i = 0; i < word.length; i++){
+        if(guessedLetter.indexOf(word[i]) > -1){
+            displayGuess += word[i] + " ";
+            x.remove(x.selectedIndex);
+        }else{
+            displayGuess += "_ ";
+        }
+    }
+    guessesLeft --;
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+    if(guessesLeft==0){
+        document.getElementById("Ltaker").innerHTML = "You're Garbage" + url;
+
+    }
+    document.getElementById("underline").innerHTML = displayGuess;
 }
 
